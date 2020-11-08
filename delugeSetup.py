b7a83ed2db1a310d50c880ff35fdf10948502d06
Alex <bigpewm@gmail.com>
2020-11-08 16:07:06 +0300

    Fix: Deluge deploy

--- delugeSetup.py
+++ delugeSetup.py
@@ -2,7 +2,6 @@ from torrentUtils import TorrentUtils
 from pathlib import Path
 
 
-baseDir = Path('app/downloads')
 auth = "root:root:10"
 authFile = Path().home().joinpath('.config/deluge/auth')
 with authFile.open('w') as f:
@@ -12,4 +11,4 @@ t = TorrentUtils()
 t.client.call('core.set_config', {'max_active_downloading': -1})
 t.client.call('core.set_config', {'max_active_limit': -1})
 t.client.call('core.set_config', {'queue_new_to_top': True})
-t.client.call('core.set_config', {'download_location': bytes(baseDir)})
+t.client.call('core.set_config', {'download_location': bytes('downloads/')})
