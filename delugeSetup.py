964a6622414e35546f141d74f406ac8129a9008d
Alex <bigpewm@gmail.com>
2020-11-15 16:31:17 +0300

    Fix: Docker Compose

--- delugeSetup.py
+++ delugeSetup.py
@@ -2,7 +2,7 @@ from torrentUtils import TorrentUtils
 from pathlib import Path
 
 
-auth = "root:root:10"
+auth = "root:root:10\n"
 authFile = Path().home().joinpath('.config/deluge/auth')
 with authFile.open('w') as f:
     f.write(auth)
@@ -11,4 +11,4 @@ t = TorrentUtils()
 t.client.call('core.set_config', {'max_active_downloading': -1})
 t.client.call('core.set_config', {'max_active_limit': -1})
 t.client.call('core.set_config', {'queue_new_to_top': True})
-t.client.call('core.set_config', {'download_location': bytes('downloads/'.encode('utf-8'))})
+t.client.call('core.set_config', {'download_location': bytes('downloads'.encode('utf-8'))})
