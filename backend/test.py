e8903b90642729d1473b47765c8dd6663b4c4994
Alex <bigpewm@gmail.com>
2021-01-18 03:58:52 +1000

    Fix: Parser API

--- backend/test.py
+++ backend/test.py
@@ -7,7 +7,6 @@ import api
 # print(t.getTorrents())
 # for c in configs:
 #     print(f'{c} - {configs[c]}')
-api.PARAMS = {'params': {'imdb_id': 'tt0088247'}}
 
-print(api.getSubtitles())
+print(api.getGenres())
 
