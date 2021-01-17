8e7b749efd6a18471c723ce00777eef907f6f51b
Alex <bigpewm@gmail.com>
2021-01-18 01:15:48 +1000

    Features: Database && User controllers functionality

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,9 @@
 from flasgger import LazyString
 from flask import request
 
+SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 JSONIFY_PRETTYPRINT_REGULAR = False
 SWAGGER = {
     'config': {
