160de9f7fdeea6bd49e5ae9d7712f80e41fb2a70
Alex <bigpewm@gmail.com>
2021-01-31 21:42:04 +0300

    Fature: Many new User Controllers, Sessions

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,9 @@
-from flask import request, abort
+from flask import request, abort, session
 import requests
 from torrentUtils import TorrentUtils
 from functools import reduce
 from database import updateDb, updateDbByDict, deleteById, checkDataDb, User, db
+from globalUtils import createHash
 
 
 # ^H в ключе - отправка заголовком
@@ -25,7 +26,7 @@ def getData(url, pointer=None):
     except RuntimeError:  # Выставляем отрицательный флаг отсутсвия контекста,
         # если функция была вызвана в тестовом режиме
         context = False
-    data = {'params': request.get_json() if request.get_data() else dict(request.args)} if context else PARAMS
+    data = {'params': getParams(request)} if context else PARAMS
     domain = findAPI(url)
     if domain in API_MAP:
         # Исполняем дополнительные инструкции для API, содержащегося в карте
@@ -47,6 +48,10 @@ def getData(url, pointer=None):
         'message':  'Success'
     }
     return response
+
+
+def getParams(r):
+    return r.get_json() if r.get_data() else dict(r.args)
 
 
 def findAPI(url):
@@ -179,20 +184,20 @@ def getDataRecursive(dataDict, mapList):
 
 
 def createUser():
-    data = dict(request.args)
+    data = getParams(request)
+    data['password'] = createHash(data['password'])
     userId = updateDbByDict(data, User, insert=True)
     return {'id': userId}
 
 
 def getUser():
-    data = dict(request.args)
+    data = getParams(request)
     if 'login' not in data:
         return {'error': 1, 'message': 'Login must be filled'}
     login = data['login']
-    try:
-        user = User.query.filter_by(login=login).one()
-    except:
-        return {'error': 1, 'message': 'User not exist'}
+    user = User.query.filter_by(login=login).first()
+    if not user:
+        return createAnswer('User not Exist', True)
     userInfo = {
         'error': 0,
         'firstName': user.firstName,
@@ -203,23 +208,57 @@ def getUser():
 
 
 def changeUser():
-    data = dict(request.args)
-    return {'message': 'Not authed'}
+    data = getParams(request)
+    if 'login' in session:
+        login = session['login']
+        user = User.query.filter_by(login=login).first()
+        updateDbByDict(data, user)
+        return createAnswer('Info change successful')
+    else:
+        return createAnswer('Not Authed', True)
 
 
 def checkLoginExist():
-    data = dict(request.args)
+    data = getParams(request)
     if 'login' not in data:
         return {'error': 1, 'message': 'Login must be filled'}
     login = data['login']
     user = checkDataDb(db.session.query(User).filter_by(login=login))
-    return {'exist': True if user else False}
+    return createAnswer('Login exist') if user else createAnswer('Login vacant')
 
 
 def checkEmailExist():
-    data = dict(request.args)
+    data = getParams(request)
     if 'email' not in data:
         return {'message': 'Email must be filled'}
     email = data['email']
     user = checkDataDb(db.session.query(User).filter_by(email=email))
-    return {'exist': True if user else False}
+    return createAnswer('Email exist') if user else createAnswer('Email vacant')
+
+
+def authUser():
+    data = getParams(request)
+    if 'login' not in data:
+        return createAnswer('Login must be filled', True)
+    login = data['login']
+    password = createHash(data['password'])
+    user = User.query.filter_by(login=login, password=password).first()
+    if user:
+        session['login'] = login
+    return createAnswer('Authed') if user else createAnswer('Login or password incorrect', True)
+
+
+def logoutUser():
+    if 'login' in session:
+        del session['login']
+    return createAnswer('Logout complete')
+
+
+def checkAuth():
+    return createAnswer('Authed') if 'login' in session else createAnswer('Not Authed', True)
+
+
+def createAnswer(message, err=False):
+    return {'error': 1 if err else 0, 'message': message}
+
+
--- backend/app.py
+++ backend/app.py
@@ -108,6 +108,24 @@ def checkEmailExist():
     return jsonify(api.checkEmailExist())
 
 
+@app.route('/user/auth', methods=['POST'])
+@swag_from('spec/user-auth.yml')
+def authUser():
+    return jsonify(api.authUser())
+
+
+@app.route('/user/auth', methods=['GET'])
+@swag_from('spec/user-check-auth.yml')
+def checkAuthUser():
+    return jsonify(api.checkAuth())
+
+
+@app.route('/user/logout', methods=['GET'])
+@swag_from('spec/user-logout.yml')
+def logoutUser():
+    return jsonify(api.logoutUser())
+
+
 if __name__ == '__main__':
     app.run(**globalUtils.addressInit(), threaded=True)
 
