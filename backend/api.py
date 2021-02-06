4268d7f9ca1c49794923a0a01d62b616fb16afc4
Alex <bigpewm@gmail.com>
2021-02-06 19:37:07 +0300

    Feature: Subtitles

--- backend/api.py
+++ backend/api.py
@@ -195,7 +195,8 @@ def findMetaFiles(resDict, filePath):
     if findVideo(filePath):
         resDict.update({'videoPath': filePath})
     if findSubtitles(filePath):
-        resDict.update({'subtitlesPath': filePath})
+        resDict.update({'subtitlesPath': [{'en': 'test_EN_path.srt', 'fr': 'test_FR_path.srt'}]})
+        # resDict.update({'subtitlesPath': filePath})
     return resDict
 
 
